d3588497c48b2a0e967fe673cca2a064aae4535b
smoeller <soeren_kai.moeller@fh-bielefeld.de>
2018-11-30 16:42:42 +0100

    Add Klu to Homotopy Do not work yet

--- SimulationRuntime/c/simulation/solver/nonlinearSolverHomotopy.c
+++ SimulationRuntime/c/simulation/solver/nonlinearSolverHomotopy.c
@@ -56,6 +56,9 @@
 #include "nonlinearSolverHomotopy.h"
 #include "nonlinearSolverHybrd.h"
 
+#include "suitesparse/Include/amd.h"
+#include "suitesparse/Include/klu.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
